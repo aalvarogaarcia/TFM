7ff54a40f636a1dd086380ee45827ac9347d472c
aalvarogaarcia <162442684+aalvarogaarcia@users.noreply.github.com>
2024-04-25 14:31:39 +0100

    Update of ProteinsPhenotypes Script

--- Script/ProteinsPhenotypes-Studies.R
+++ Script/ProteinsPhenotypes-Studies.R
@@ -1,21 +1,29 @@
 
 # Libraries
 library(tidyverse)
+library(moments) #Kurtosis &Skewness
 library(dummy)
 library(ggplot2)
 library(umap)
 library(dbscan)
 library(ConsensusClusterPlus)
 
+
 set.seed(789)
+
+
+
+
 #### DATA INGESTION ####
 
 #Working directory
   rm(list = ls())
   getwd()
-  path = "/Users/alvarogarciamunoz/Documents/GitHub/TFM-2.0"
+  path = "/Users/alvarogarciamunoz/Documents/DATOS"
   setwd(path)
 
+  
+  
 #File reading
   path <- "Data"
   fn_phenotypes<- "phenotypes.tsv"
@@ -25,57 +33,177 @@ set.seed(789)
   proteins <- read.csv(paste(path,fn_proteins,sep="/"))
   
   
+  
+  
+  
 #### DATA AGGREGATION ####
   
-#Phenotype chr management
-  temp <- dummy(phenotypes[,-1], int = TRUE)
-  temp1 <- scale(cbind(phenotypes[,-ncol(phenotypes)], temp)[,-1])
-  temp2 <- cbind(phenotypes[,-ncol(phenotypes)], temp)[,-1]
+#EXPLORATORY DATA ANALYSIS#
   
+#UNIVARIANT
+  
+  
+  
+  #Search NA's
+  summary(phenotypes) #Phenotypes  
+  
+  for(i in 1:ncol(proteins)){ #Boucle for Proteins
+    NA.index <- which(is.na(proteins[,i]))
+    if(length(NA.index) != 0){
+      print(paste('The protein', colnames(proteins)[i], 'has a missing value in index', NA.index, sep = ' '))
+    }
+  }
+
+#NA correction or elimination?  
 
   
-  #Phenotype normalization analysis
-  par(mfrow = c(3,3))
-  for (x in 2:ncol(temp1)){
-    plot(density(temp1[,x]), main = paste("Density of phenotype", colnames(temp1)[x], sep = " "))
+  
+  #Phenotypes plotting
+  par(mfrow = c(2,2))
+  temp <- phenotypes[, -which(colnames(phenotypes)=='Age')] #Age is the only phenotype with a non-discrete distribution.
+  
+  for(i in 2:ncol(temp)){
+    tempt <- table(temp[,i]) #Table of phenotype
+    print(tempt) #Obtain table
+    print(prop.table(tempt)) #Obtain prop table
+    barplot(tempt, main = paste('Variable', colnames(temp)[i]),
+            xlab = 'Levels', ylab = 'Quantity') #Shows a barplot
   }
   
-  par(mfrow = c(3,3))
-  for (x in 2:ncol(temp2)){
-    plot(density(temp2[,x]), main = paste("Density of phenotype", colnames(temp2)[x], sep = " "))
-  }
-  
-  #The structure stand, lets use normalized
-  phenotypes.int<- cbind(phenotypes[,1], temp1)
+  par(mfrow = c(1,1))
+  temp <-phenotypes[, which(colnames(phenotypes) == 'Age')]
+  plot(density(temp$Age), main = 'Density of variable Age')
   
   
   
-#Proteins Plotting
-  index <- sample(1:ncol(proteins), 16, replace = FALSE)
-
-  temp <- proteins[,index]
+  #Continue distribution analysis
+  
+  temp <- phenotypes[, which(colnames(phenotypes) == 'Age')]
+  index <- sample(2:ncol(proteins), 15, replace = FALSE)
+  
+  temp <- cbind(proteins[,index], temp)
   par(mfrow=c(4,4))
+  
+    #Density (sample)
   for (x in 1:ncol(temp)){
     plot(density(temp[,x]), main = paste("Density of protein number", colnames(temp)[x], sep = " "))
   }
   
-  rm(temp)
+  
+    #Quartiles, Mean, MAD, CV & Outliers (sample)
+  for (i in 1:ncol(temp)){ 
+    tempt <- fivenum(temp[,i])
+    q <- paste('-The quartiles of ', colnames(temp)[i], 
+               '. Min:',tempt[1], 
+               '. 1Q:' ,tempt[2],
+               '. Median:', tempt[3],
+               '. 3Q:', tempt[4],
+               '. Máx:', tempt[5]
+               , sep = '')
+    m <- paste('-The mean of', colnames(temp)[i], 'is', mean(temp[,i], sep= ' '))
+    mad <- paste("-The median absolute deviation of",colnames(temp)[i], "is", mad(temp[,i]), sep= " ")
+    
+    tempt <- sd(temp[,i])/mean(temp[,i])
+    cv <- paste("-The coefficient of variation of",colnames(temp)[i], "is", tempt, sep= " ")
+    title <- paste('Analysis of variable ', colnames(temp)[i], ':', sep = '')
+    cat(title ,q, m, mad, cv, sep = "\n")   
 
+    boxplot(temp[,i], main = paste("Boxplot of Variable", colnames(temp)[i], sep = " "))
+  } #We don't exclude the outliers, we need to study them...
+  
+  
+    #Kurtosis & Skewness (all)
+  kurtosis <- list('Normal' = c(), 'Leptokurtic' = c(), 'Platykurtic' = c())
+  skewness <- list('Symmetric' = c(), 'Right' = c(), 'Left' = c())
+  for (i in 2:ncol(proteins)){
+    temp <- kurtosis(proteins[,i])
+    tempt <- skewness(proteins[,i])
+    
+    if(temp == 3){
+      kurtosis[[1]] <- cbind(kurtosis[[1]], colnames(proteins)[i])
+    }
+    else if(temp >3){
+      kurtosis[[2]] <- cbind(kurtosis[[2]], colnames(proteins)[i])
+    }
+    else{
+      kurtosis[[3]] <- cbind(kurtosis[[3]], colnames(proteins)[i])
+    }
+    
+    
+    if(temp == 0){
+      skewness[[1]] <- cbind(skewness[[1]], colnames(proteins)[i])
+    }
+    else if(temp >0){
+      skewness[[2]] <- cbind(skewness[[2]], colnames(proteins)[i])
+    }
+    else{
+      skewness[[3]] <- cbind(skewness[[3]], colnames(proteins)[i])
+    }
+  }
+  
+  N <- paste(kurtosis[[1]], sep = ", ")
+  L <- paste(kurtosis[[2]], sep = ", ")
+  P <- paste(kurtosis[[3]], sep = ", ")
+  
+  cat('Kurtosis lengths:', 'Normal:', length(kurtosis[[1]]), 'Leptokurtic:', length(kurtosis[[2]]), 'Platykurtic:', length(kurtosis[[3]]), sep = '\n')
+  
+  cat('Kurtosis results:', 'Normal: ', N, 'Leptokurtic: ', L, 'Platykurtic: ', P)
+  
+  N <- paste(skewness[[1]], sep = ", ")
+  L <- paste(skewness[[2]], sep = ", ")
+  P <- paste(skewness[[3]], sep = ", ")
+  
+  cat('Skewness lengths:', 'Symmetric:', length(skewness[[1]]), 'Right:', length(skewness[[2]]), 'Left:', length(skewness[[3]]), sep = '\n')
+  
+  cat('Skewness results:', 'Symmetric: ', N, 'Rigth: ', L, 'Left: ', P)
+  
+  #BIVARIATE#
+  #Proteins Covariance
+  temp <- cov(proteins[,-1])
+  temp2 <- eigen(temp)
+  newbase <- temp2$vectors
+  m<-c()
+  for (i in 1:nrow(proteins)){
+    pr <- newbase * proteins[i,-1]
+    m <- rbind(m, pr)
+  }
+  
+  proteins.nb <-cbind("id" = proteins$id, m) #No covariance base proteins
+  
+  temp <- phenotypes[, which(colnames(phenotypes) == 'Age')]
+  index <- sample(2:ncol(proteins.nb), 15, replace = FALSE)
+  
+  temp <- cbind(proteins.nb[,index], temp)
+  par(mfrow=c(4,4))
+  
+  #Density (sample)
+  for (x in 1:ncol(temp)){
+    plot(density(temp[,x]), main = paste("Density of protein number", colnames(temp)[x], sep = " "))
+  }
+  
+  tempt <- sample(1, 1:ncol(proteins))
+  model <- lm(colnames(proteins)[, tempt]~.)
+  
+  #Phenotype chr management
+  temp <- dummy(phenotypes[,-1], int = TRUE)
+  temp2 <- cbind(phenotypes[,-ncol(phenotypes)], temp)[,-1]
+  
+  
+  #The structure stand, lets use normalized
+  phenotypes.int<- cbind(phenotypes[,1:ncol(phenotypes)-1], temp)
 
 #Data grouping - Labels, em, Data. (Labels = dependent variable, em = extended matrix, Data = independent variables )
 
   labels <- subset(phenotypes.int, select = c(id, Diagnosis))
   labels$Diagnosis <- as.factor(labels$Diagnosis)
   
-  em <- merge(proteins, phenotypes.int, by="id") 
+  em <- merge(proteins.nb, phenotypes.int, by="id") 
   data <- em[,-which(colnames(em)=="Diagnosis")]
 
 #Normalize
   em.norm <-scale(em[,-ncol(proteins)])
   data.norm <-scale(data[,-ncol(proteins)])
   
-  
-#### EXPLORATORING DATA ANALYSIS ####
   
 #Normal
   
@@ -84,13 +212,41 @@ set.seed(789)
 #### PIPELINE A ####
   
 #UMAP 1
-  em.umap <-umap(em.norm)
+  em.split <- split(em, em$Diagnosis)
+  
+  em.umap <-umap(em[,-1])
   data.umap <-umap(data.norm)
   proteins.umap <- umap(proteins[,-1])
   
   coordinates.em.umap <- data.frame(em.umap$layout, labels = labels$Diagnosis)
   coordinates.data.umap <- data.frame(data.umap$layout, labels = labels$Diagnosis)
   coordinates.proteins.umap <-data.frame(proteins.umap$layout, labels = labels$Diagnosis)
+  
+    di1<- umap(em.split[[1]][,-1])
+    di2<- umap(em.split[[2]][,-1])
+    di3<- umap(em.split[[3]][,-1])
+    di4<- umap(em.split[[4]][,-1])
+  
+  coordinates.di1 <-data.frame(di1$layout, labels = em.split[[1]]$Diagnosis)
+  
+  ggplot(data = coordinates.di1, aes(X1, X2, color = labels)) + geom_point() + theme_light() +
+    labs(x="Axis x", y="Axis y", title="UMAP Reduction of Extended Matrix")
+  
+  
+  coordinates.di2 <-data.frame(di2$layout, labels = em.split[[2]]$Diagnosis)
+  
+  ggplot(data = coordinates.di2, aes(X1, X2, color = labels)) + geom_point() + theme_light() +
+    labs(x="Axis x", y="Axis y", title="UMAP Reduction of Extended Matrix")
+  
+  coordinates.di3 <-data.frame(di3$layout, labels = em.split[[3]]$Diagnosis)
+  
+  ggplot(data = coordinates.di3, aes(X1, X2, color = labels)) + geom_point() + theme_light() +
+    labs(x="Axis x", y="Axis y", title="UMAP Reduction of Extended Matrix")
+  
+  coordinates.di4 <-data.frame(di4$layout, labels = em.split[[4]]$Diagnosis)
+  
+  ggplot(data = coordinates.di4, aes(X1, X2, color = labels)) + geom_point() + theme_light() +
+    labs(x="Axis x", y="Axis y", title="UMAP Reduction of Extended Matrix")
   
 #Plotting 1st 3 matrix
   
@@ -110,11 +266,11 @@ set.seed(789)
 #HDBSCAN. Unsupervised - Clustering
   
   #Clusters
-  em.hdbscan <- hdbscan(em.norm, minPts = 3)
+  em.hdbscan <- hdbscan(em[,-1], minPts = 5)
   data.hdbscan <- hdbscan(data.norm, minPts = 3)
-  proteins.hdbscan <- hdbscan(proteins[,-1], minPts = 3)
+  proteins.hdbscan <- hdbscan(proteins[,-1], minPts = 5)
   
-  coordinates.em.hdbscan <- data.frame(em.umap$layout, labels = as.factor(em.hdbscan$cluster))
+  coordinates.em.hdbscan <- data.frame(proteins.umap$layout, labels = as.factor(em.hdbscan$cluster))
   coordinates.data.hdbscan <- data.frame(data.umap$layout, labels = as.factor(data.hdbscan$cluster))
   coordinates.proteins.hdbscan <- data.frame(proteins.umap$layout, labels = as.factor(proteins.hdbscan$cluster))
     
